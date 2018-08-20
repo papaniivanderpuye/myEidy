81dc3646517dd23a75773a31f71bb7ecf63c18ca
papaniivanderpuye <papaniivanderpuye@gmail.com>
2018-08-19 21:35:24 -0400

    improvements

--- src/DrawButton/DrawButton.jsx
+++ src/DrawButton/DrawButton.jsx
@@ -15,7 +15,7 @@ class DrawButton extends Component{
   render(props){
     return(
       <div className= "buttonContainer">
-        <button className = "btn" onClick= {this.drawCard}>Draw Card </button>
+        <button className="button primary" onClick= {this.drawCard}>Draw Card </button>
       </div>
     )
   }
